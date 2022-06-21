a8c5d4246035c3bbd741dde90323aa5afbc4d855
Giulia <giuliafloris88@gmail.com>
2022-06-21 11:01:25 +0200

    update 2

--- DataFetch/ContentView.swift
+++ DataFetch/ContentView.swift
@@ -11,27 +11,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var exp: String = ""
     var body: some View {
-        Text(exp)
-        Button {
-            Task {
-                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://x-math.herokuapp.com/api/random")!)
-                let decodedResponse = try? JSONDecoder().decode(mathf.self, from: data)
+        
+        ZStack{
+            
+            Image("background").resizable().frame(width: 500, height: 1000)
+            
+            VStack{
+                Text(exp).font(Font.custom("Montserrat-Bold", size: 20.0))
+                Button {
+                    
+                    
+                    Task {
+                        let (data, _) = try await URLSession.shared.data(from: URL(string:"https://x-math.herokuapp.com/api/random")!)
+                        let decodedResponse = try? JSONDecoder().decode(mathf.self, from: data)
 
-                let a = "\(decodedResponse?.expression ?? " ") = "
-                let b = decodedResponse?.answer ?? 0
-                exp = " \(String(describing: a)) \(String(describing: b)) "
+                        let a = "\(decodedResponse?.expression ?? " ") = "
+                        let b = decodedResponse?.answer ?? 0
+                        exp = " \(String(describing: a)) \(String(describing: b)) "
+              
+                         
+                    }
+                } label: {
+                    Text("Nuova Espressione").font(Font.custom("Montserrat-Bold", size: 30.0)).foregroundColor(.green)
+            }.offset(x: 0, y: 300)
+
                 
                 
-                /*
-                exp = ("\(decodedResponse?.expression) \(decodedResponse?.answer)") ?? ""
-                */
-                 
-                 
-                 
-                 
-            }
-        } label: {
-            Text("Nuova Espressione")
+        }
+
         }
     }
 }
