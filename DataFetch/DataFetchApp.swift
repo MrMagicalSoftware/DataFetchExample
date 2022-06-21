d0f4a14462b16500487415faae7e004a1c5addce
Giulia <giuliafloris88@gmail.com>
2022-06-21 10:08:13 +0200

    Initial Commit

---
+++ DataFetch/DataFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFetchApp.swift
+//  DataFetch
+//
+//  Created by user on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DataFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
