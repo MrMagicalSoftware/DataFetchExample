2e9d91222e4e1b5dc344dfbfbda7f264d8b36a2e
Giulia <giuliafloris88@gmail.com>
2022-06-21 10:50:28 +0200

    update 1

--- DataFetch/ContentView.swift
+++ DataFetch/ContentView.swift
@@ -5,17 +5,49 @@
 //  Created by user on 21/06/22.
 //
 
-import SwiftUI
 
+import Foundation
+import SwiftUI
 struct ContentView: View {
+    @State private var exp: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(exp)
+        Button {
+            Task {
+                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://x-math.herokuapp.com/api/random")!)
+                let decodedResponse = try? JSONDecoder().decode(mathf.self, from: data)
+
+                let a = "\(decodedResponse?.expression ?? " ") = "
+                let b = decodedResponse?.answer ?? 0
+                exp = " \(String(describing: a)) \(String(describing: b)) "
+                
+                
+                /*
+                exp = ("\(decodedResponse?.expression) \(decodedResponse?.answer)") ?? ""
+                */
+                 
+                 
+                 
+                 
+            }
+        } label: {
+            Text("Nuova Espressione")
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+struct mathf: Codable {
+    
+    let expression: String
+    let answer: Int
+}
+
+
+
+
+
+
